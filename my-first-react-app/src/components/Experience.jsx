65ba9f70cfbec6c2fa316d5d9f3e6183684ca2b6
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-21 00:33:18 +0530

    add Experience component with form handling

--- my-first-react-app/src/components/Experience.jsx
+++ my-first-react-app/src/components/Experience.jsx
@@ -3,4 +3,47 @@ import Section from "./Section";
 
 export default function Experience() {
 
+    const [company, setCompany] = useState("");
+    const [position, setPosition] = useState("");
+    const [responsibilities, setResponsibilities] = useState("");
+    const [duration, setDuration] = useState("");
+
+    const [submitted, setSubmitted] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setSubmitted(true);
+    }
+
+    function handleEdit() {
+        setSubmitted(false);
+    }
+
+    return (
+        <Section
+        title="Experience"
+        handleSubmit={handleSubmit}
+        submitted={submitted}
+        handleEdit={handleEdit}
+
+        displayData = {
+            <div>
+                <p>company name: {company}</p>
+                <p>position: {position}</p>
+                <p>Main Responsibilities: {responsibilities}</p>
+                <p>Duration of Work: {duration}</p>
+            </div>
+        }
+        >
+            <input type="text" placeholder="Company" value={company} onChange={(e) => setCompany(e.target.value)} />
+            <input type="text" placeholder="Position" value={position} onChange={(e) => setPosition(e.target.value)} />
+            <textarea  placeholder="I build websites..." value={responsibilities} onChange={(e) => setResponsibilities(e.target.value)} />
+            <input type="text" placeholder="Duration" value={duration} onChange={(e) => setDuration(e.target.value)} />
+
+
+
+        </Section>
+
+    )
+
 }
