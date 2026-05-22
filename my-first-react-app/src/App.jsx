8c172f7eb39c1d8700b2cf565d86cedd8f0b4e2a
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-22 23:51:31 +0530

    remove header from App component and add Navbar component for better structure

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
 
   return (
     <div className="app">
-      <h1>CV Application</h1>
       <GeneralInfo />
       <Educational />
       <Experience /> 
---
+++ my-first-react-app/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h2 className="navbar-title">CV Application</h2>
+
+            <button className="navbar-button">Download CV</button>
+        </nav>
+    );
+}
