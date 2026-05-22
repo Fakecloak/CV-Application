ffed017e3e75511d3382e6934641abda941a06ff
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-22 23:51:49 +0530

    import Navbar component to App

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import GeneralInfo from './components/GeneralInfo';
 import Educational from './components/Educational';
 import Experience from './components/Experience';
+import Navbar from './components/Navbar';
 import './style.css';
 
 function App() {
